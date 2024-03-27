77fa83c27f31865aa2b6b1c1dd56ca2d239e356e
Amit Shwartz <shwartzamit17@gmail.com>
2024-03-27 19:14:49 +0200

    syntax

--- Backend/src/controllers/neighborhoods.ctrl.ts
+++ Backend/src/controllers/neighborhoods.ctrl.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import neighborhoodService from "../services/neighborhoods.service";
 import { InternalServerError, handleZodError } from "../utils/ApiError";
-import { asyncWrapper } from "../utils/asyncWrraper";
+import { asyncWrapper } from "../utils/asyncWrapper";
 import { GetNeighborhoodsBodySchema } from "../validators/neighborhood.schema";
 
 class NeighborhoodController {
   getNeighborhoods = asyncWrapper(async (req: Request, res: Response) => {
+    console.log("getNeighborhoods", JSON.stringify(req.query));
     //ageRange=[minAge,maxAge]`,`maxDistance=[distance]km`, and `sortBy=[field,order]
     try {
       const queryBody = GetNeighborhoodsBodySchema.parse(req.query);
