5d0eab61094e97dd1849e284285f65190db33db3
Amit Shwartz <shwartzamit17@gmail.com>
2024-03-28 18:45:23 +0200

    neighborhoodService

---
+++ Frontend/src/services/neighborhood.service.ts
@@ -0,0 +1,27 @@
+import neighborhoodApi from "../api/neighborhood/neighborhood.api";
+import INeighborhoodApi, {
+  FetchNeighborhoodsParams,
+  sortOption,
+} from "../api/neighborhood/neighborhood.api.interface";
+import { Neighborhood } from "../types";
+
+class NeighborhoodService {
+  private api: INeighborhoodApi;
+  constructor(api: INeighborhoodApi) {
+    this.api = api;
+  }
+
+  async fetchNeighborhoods(
+    params: FetchNeighborhoodsParams
+  ): Promise<Neighborhood[]> {
+    return (await this.api.fetchNeighborhoods(params)).data;
+  }
+
+  async fetchNeighborhoodSortOptions(): Promise<sortOption> {
+    return (await this.api.fetchNeighborhoodSortOptions()).data;
+  }
+}
+
+const neighborhoodService = new NeighborhoodService(neighborhoodApi);
+
+export default neighborhoodService;
