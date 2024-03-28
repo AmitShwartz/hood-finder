427821f168690ba001d64d54357ed3d3e38cabb6
Amit Shwartz <shwartzamit17@gmail.com>
2024-03-28 11:32:59 +0200

    replace Frontend with expo

---
+++ Frontend/ios/HomeAssignment/HomeAssignment/HomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssignmentApp.swift
+//  HomeAssignment
+//
+//  Created by Amit Shwartz on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
