0279c6879f38cf6c797e4e583ea94370f30aee57
Amit Shwartz <shwartzamit17@gmail.com>
2024-03-28 11:33:15 +0200

    replace Frontend with expo

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import HoodFinder from './src/HoodFinder/HoodFinder';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-
-function App(): JSX.Element {
-  return <HoodFinder />
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
